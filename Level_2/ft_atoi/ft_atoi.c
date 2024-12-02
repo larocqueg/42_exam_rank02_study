de2135206877835726f1506d04fdebcedfecc541
larocqueg <portugalgabriel2021@gmail.com>
2024-12-02 23:18:51 +0000

    ft_atoi done

--- Level_2/ft_atoi/ft_atoi.c
+++ Level_2/ft_atoi/ft_atoi.c
@@ -17,20 +17,24 @@ int	ft_atoi(const char *str);
 int	ft_atoi(const char *str)
 {
 	int	i;
+	int	sign;
 	int	result;
 
 	i = 0;
 	result = 0;
+	sign = 1;
 	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
 		i++;
 	if (str[i] == '+' || str[i] == '-')
 	{
 		if (str[i] == '-')
-			write(1, '-', 1);
+			sign = -1;
 		i++;
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		result = result * 10 + '0';
+		result = result * 10 + (str[i] - '0');
+		i++;
 	}
+	return (result *sign);
 }
