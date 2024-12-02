c998d442f4fc3d5aae8ce3f471cadad43c41e284
larocqueg <portugalgabriel2021@gmail.com>
2024-12-02 17:32:23 +0000

    started atoi

---
+++ Level_2/ft_atoi/ft_atoi.c
@@ -0,0 +1,36 @@
+/*
+Assignment name  : ft_atoi
+Expected files   : ft_atoi.c
+Allowed functions: None
+--------------------------------------------------------------------------------
+
+Write a function that converts the string argument str to an integer (type int)
+and returns it.
+
+It works much like the standard atoi(const char *str) function, see the man.
+
+Your function must be declared as follows:
+
+int	ft_atoi(const char *str);
+*/
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	result;
+
+	i = 0;
+	result = 0;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			write(1, '-', 1);
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + '0';
+	}
+}
