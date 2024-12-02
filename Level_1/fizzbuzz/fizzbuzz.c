8e0c81c022f4d5e79fc41e51153897a9d47ed09d
larocqueg <portugalgabriel2021@gmail.com>
2024-12-02 14:07:49 +0000

    f

---
+++ Level_1/fizzbuzz/fizzbuzz.c
@@ -0,0 +1,76 @@
+/*
+Assignment name  : fizzbuzz
+Expected files   : fizzbuzz.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that prints the numbers from 1 to 100, each separated by a
+newline.
+
+If the number is a multiple of 3, it prints 'fizz' instead.
+
+If the number is a multiple of 5, it prints 'buzz' instead.
+
+If the number is both a multiple of 3 and a multiple of 5, it prints 'fizzbuzz' instead.
+
+Example:
+
+$>./fizzbuzz
+1
+2
+fizz
+4
+buzz
+fizz
+7
+8
+fizz
+buzz
+11
+fizz
+13
+14
+fizzbuzz
+[...]
+97
+98
+fizz
+buzz
+$>
+*/
+
+#include <unistd.h>
+
+void	ft_putnbr(int n);
+void	ft_putchar(char c);
+
+int	main(void)
+{
+	int	i = 1;
+	while (i <= 100)
+	{
+		if (i % 15 == 0)
+			write(1, "fizzbuzz", 8);
+		else if (i % 5 == 0)
+			write(1, "buzz", 4);
+		else if (i % 3 == 0)
+			write(1, "fizz", 4);
+		else
+			ft_putnbr(i);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int n)
+{
+	if (n > 9)
+		ft_putnbr(n / 10);
+	ft_putchar((n % 10) + '0');
+}
