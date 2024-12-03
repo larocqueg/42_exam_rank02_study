a050e935dcd193d3a26149478fe3471ae7921dcc
larocqueg <portugalgabriel2021@gmail.com>
2024-12-03 11:15:48 +0000

    ft_strdup done

---
+++ Level_2/ft_strdup/ft_strdup.c
@@ -0,0 +1,35 @@
+/*
+Assignment name  : ft_strdup
+Expected files   : ft_strdup.c
+Allowed functions: malloc
+--------------------------------------------------------------------------------
+
+Reproduce the behavior of the function strdup (man strdup).
+
+Your function must be declared as follows:
+
+char    *ft_strdup(char *src);
+*/
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int	i;
+	char	*dup;
+
+	i = 0;
+	while (src[i])
+		i++;
+	dup = (char *)malloc(sizeof(char) * (i + 1));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
