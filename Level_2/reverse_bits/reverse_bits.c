b41b874efa5b9fa63b8a1d4100240afb530100b3
larocqueg <portugalgabriel2021@gmail.com>
2024-12-05 12:07:34 +0000

    reversed_bits done

---
+++ Level_2/reverse_bits/reverse_bits.c
@@ -0,0 +1,35 @@
+/*
+Assignment name  : reverse_bits
+Expected files   : reverse_bits.c
+Allowed functions:
+--------------------------------------------------------------------------------
+
+Write a function that takes a byte, reverses it, bit by bit (like the
+example) and returns the result.
+
+Your function must be declared as follows:
+
+unsigned char	reverse_bits(unsigned char octet);
+
+Example:
+
+  1 byte
+_____________
+ 0100  0001
+     ||
+     \/
+ 1000  0010
+*/
+
+unsigned char	reverse_bits(unsigned char octet)
+{
+	int	i = 0;
+	unsigned char rev = 0;
+	while (i < 8)
+	{
+		rev = (rev << 1) | (octet & 1);
+		octet >>=1;
+		i++;
+	}
+	return (rev);
+}
