68c68a96ebd828d2a44958c4fabb7a2f31b437cb
larocqueg <portugalgabriel2021@gmail.com>
2024-12-06 11:58:46 +0000

    put_hex done

---
+++ Level_3/print_hex/print_hex.c
@@ -0,0 +1,71 @@
+/*
+Assignment name  : print_hex
+Expected files   : print_hex.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a positive (or zero) number expressed in base 10,
+and displays it in base 16 (lowercase letters) followed by a newline.
+
+If the number of parameters is not 1, the program displays a newline.
+
+Examples:
+
+$> ./print_hex "10" | cat -e
+a$
+$> ./print_hex "255" | cat -e
+ff$
+$> ./print_hex "5156454" | cat -e
+4eae66$
+$> ./print_hex | cat -e
+$
+*/
+
+#include <unistd.h>
+#include <stdio.h>
+
+int		ft_atoi(char *str);
+void	ft_puthex(unsigned long n);
+
+int	main(int ac, char **av)
+{
+	int n;
+	if (ac == 2)
+	{
+		n = ft_atoi(av[1]);
+		if (n >= 0)
+			ft_puthex((unsigned long)n);
+		write(1, "\n", 1);
+		return (0);
+	}
+	write(1, "\n", 1);
+	return (1);
+}
+
+void	ft_puthex(unsigned long n)
+{
+	char	*base = "0123456789abcdef";
+
+	if (n >= 16)
+		ft_puthex(n / 16);
+	write(1, &base[n % 16], 1);
+}
+
+int	ft_atoi(char *str)
+{
+	int	i = 0;
+	int	sign = 1;
+	int	result = 0;
+
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign -= sign;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+		result = result * 10 + (str[i++] - '0');
+	return (result * sign);
+}
