8dee77e961e84e9426691144c922d0a997ddd9d6
Gabriel La Rocque <127416900+larocqueg@users.noreply.github.com>
2024-12-10 10:58:23 +0000

    Update ft_atoi_base.c (passes on grademe)

--- Level_3/ft_atoi_base/ft_atoi_base.c
+++ Level_3/ft_atoi_base/ft_atoi_base.c
@@ -23,29 +23,31 @@ int	ft_atoi_base(const char *str, int str_base);
 
 int	ft_atoi_base(const char *str, int str_base)
 {
-	int		i = 0;
-	int		sign = 1;
-	int		digit = 0;
-	long	result = 0;
-
-	if (str_base < 2 || str_base > 16 || !str)
+	int result = 0;
+	int	digit;
+	int sign = 1;
+	int	i = 0;
+	if (str_base < 2 || str_base > 16)
 		return (0);
-	while (str[i] == ' ' || str[i] == '\t')
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
 		i++;
 	if (str[i] == '+' || str[i] == '-')
 	{
 		if (str[i] == '-')
-			sign = -sign;
+			sign = -1;
 		i++;
 	}
+	digit = 0;
 	while (str[i])
 	{
 		if (str[i] >= '0' && str[i] <= '9')
-			digit += str[i] - '0';
+			digit = str[i] - '0';
 		else if (str[i] >= 'a' && str[i] <= 'f')
-			digit += str[i] - 'a' + 10;
+			digit = str[i] - 'a' + 10;
 		else if (str[i] >= 'A' && str[i] <= 'F')
-			digit += str[i] - 'A' + 10;
+			digit = str[i] - 'A' + 10;
+		else
+			break ;
 		if (digit >= str_base)
 			break ;
 		result = result * str_base + digit;
